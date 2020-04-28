9a41ff2d83861d115aa69b8b84b2683761bf82bb
Javier Ortuño Roig <javi_roig@hotmail.es>
2020-04-28 23:51:37 +0200

    localStorage

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,44 @@
 let numeroDeTareas = 0;
 
+let todo = {
+    list: []
+}
+
+window.addEventListener('load', function () {
+    listaAntigua();
+})
+
+
+function listaAntigua() {
+
+    todo = JSON.parse(localStorage.getItem("todo"));
+    if (todo != null) {
+        for (let i = 0; i < todo.list.length; i++) {
+            let añadir = document.getElementById("list");
+            
+            let node = document.createElement("div");
+            añadir.appendChild(node);
+            node.outerHTML = todo.list[i];
+        }
+    }
+
+}
 
 function remove(element) {
     const tarea = document.getElementById("list");
 
     let elementParent = element.parentElement;
     let grandfather = elementParent.parentElement;
+
+    for (let i = 0; i < todo.list.length; i++) {
+        if (grandfather.outerHTML === todo.list[i]) {
+            todo.list = todo.list.filter((outer) => {
+                return outer !== grandfather.outerHTML;
+            })
+
+            localStorage.setItem("todo", JSON.stringify(todo))
+        }
+    }
 
     tarea.removeChild(grandfather);
 }
@@ -29,6 +62,16 @@ function add () {
         
         
         node.innerHTML = "<div class=\"col-12\"><br></div><div class=col-md-1></div><div class=\"col-md-2 col-lg-1 pr-0\"><button class=\"btn btn-danger tam-boton\" onclick=\"remove(this)\"\"> Elimnar</button></div><div class=\"col-md-8 col-lg-9 ml-3 border border-dark\"><p class=text-center style=\"word-break: break-all\";>" + texto + "</p></div>"
+
+        if (todo === null) {
+            todo = {
+                list: [node.outerHTML]
+            }
+            localStorage.setItem("todo", JSON.stringify(todo));
+        } else {
+            todo.list.push(node.outerHTML);
+            localStorage.setItem("todo", JSON.stringify(todo));
+        }
         
         añadir.appendChild(node);
     }
